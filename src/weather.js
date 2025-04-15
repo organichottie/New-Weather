82842b347e1212d71c917901ec01252444f65930
organichottie <mmatlitech@gmail.com>
2025-04-15 14:13:41 +0200

    array for weekdays added

--- src/weather.js
+++ src/weather.js
@@ -1,9 +1,42 @@
+function updateWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let temp = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temp);
+}
+
+//Arrays for days
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+}
+
+function searchCity(city) {
+  let apiKey = "11cb60e5ofea031ab3tc8e1af4a45bbb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  // let cityElement = document.querySelector("#city");
+  // cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
