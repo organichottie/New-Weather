360427a15fdd45aaac8cee0e640d6fd4984635df
organichottie <mmatlitech@gmail.com>
2025-04-15 15:14:03 +0200

    we won yeah! arrays, elements, IDs final touches added

--- src/weather.js
+++ src/weather.js
@@ -2,8 +2,21 @@ function updateWeather(response) {
   let tempElement = document.querySelector("#temp");
   let temp = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  //replacement elements
   cityElement.innerHTML = response.data.city;
   tempElement.innerHTML = Math.round(temp);
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class = "weather-icon" />`;
 }
 
 //Arrays for days
@@ -20,6 +33,13 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
+
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
