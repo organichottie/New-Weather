ed0b792c3dd48fda71ac94911780aea25b93e429
organichottie <mmatlitech@gmail.com>
2025-04-19 13:30:29 +0200

    Array for forecast weather + loop for the days

--- src/weather.js
+++ src/weather.js
@@ -60,3 +60,28 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+
+/*Forecast*/
+
+function showForecast() {
+  let dayz = ["Tues", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  dayz.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+          <div class="weather-forecast-date">
+            <div class="weather-forecast-day">${day}</div>
+            <div class="weather-forecast-icon">⛅</div>
+            <div class="weather-forecast-temp">
+              <div class="weather-forecast-high"><strong>15°</strong></div>
+              <div class="weather-forecast-low">9°</div>
+            </div>
+            </div>
+            `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+showForecast();
